0cc89e9b92b6061792c7e27a424d6c48ad58c5d2
seunghyeonlee9661 <dltmdgus9661@gmail.com>
2024-06-17 12:48:53 +0900

    start project

---
+++ BaseballGame/src/Main.java
@@ -0,0 +1,71 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 1. 정답 생성
+		String answer = getAnswer(3);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!");
+
+		// 2. 입력 초기화
+		String input = "";
+		int cnt = 0;
+
+		// 3. 입력 확인
+		while (!input.equals(answer)) {
+			System.out.println(++cnt + "번째 시도 : ");
+			checkInput(scanner.nextLine(), answer);
+		}
+
+		// 4. 결과 출력
+		System.out.println(cnt + "번만에 맞히셨습니다.");
+		System.out.println("게임을 종료합니다.");
+	}
+
+	// 입력과 정답을 비교해 스트라이크와 볼을 출력하는 함수
+	private static void checkInput(String input, String answer) {
+		int ball = 0;
+		int strike = 0;
+
+		for (String s : input.split("")) {
+			if (answer.indexOf(s) >= 0) {
+				if (answer.indexOf(s) == input.indexOf(s))
+					strike++;
+				else
+					ball++;
+			}
+		}
+
+		if (ball == 0 && strike == 0)
+			System.out.print("0B0S");
+		else {
+			if (ball > 0)
+				System.out.print(ball + "B");
+			if (strike > 0)
+				System.out.print(strike + "S");
+		}
+		System.out.println();
+
+	}
+
+	// 중복없는 숫자 조합을 생성하는 함수
+	private static String getAnswer(int size) {
+		Random random = new Random();
+		String answer = "";
+		String number;
+		while (answer.length() < size) {
+			number = Integer.toString(random.nextInt(10));
+			for (String s : answer.split("")) {
+				if (s.equals(number)) {
+					number = "";
+					break;
+				}
+			}
+			answer = answer + number;
+		}
+		return answer;
+	}
+
+}
